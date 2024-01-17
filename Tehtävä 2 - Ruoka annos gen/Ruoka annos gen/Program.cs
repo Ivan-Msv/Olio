5d3ac12153ef7b6712f5a5d57132c5cc28320d21
Ivan-Msv <s2200853@edu.bc.fi>
2024-01-17 14:09:50 +0200

    Tehtävä 2 ja 3 valmis.

---
+++ Tehtävä 2 - Ruoka annos gen/Ruoka annos gen/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ruoka_annos_gen
+{
+    class Program
+    {
+        enum RaakaAine
+        {
+            Nautaa, Kanaa, Kasviksia
+        }
+        enum Lisuke
+        {
+            Perunaa, Riisiä, Pastaa
+        }
+        enum Kastike
+        {
+            Curry, Hapanimelä, Chili
+        }
+        static void Main(string[] args)
+        {
+            List<string> ruokaAnnos = new List<string>();
+            while (true)
+            {
+                Console.Write($"Pääraaka-Aine ({RaakaAine.Nautaa}, {RaakaAine.Kanaa}, {RaakaAine.Kasviksia}): ");
+                if (Enum.TryParse(Console.ReadLine(), true, out RaakaAine sRaakaAine))
+                {
+                    ruokaAnnos.Add(sRaakaAine.ToString());
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Väärä Pääraaka-Aine, yritä uudelleen.");
+                    continue;
+                }
+            }
+            while (true)
+            {
+                Console.Write($"Lisukkeet ({Lisuke.Perunaa}, {Lisuke.Riisiä}, {Lisuke.Pastaa}): ");
+                if (Enum.TryParse(Console.ReadLine(), true, out Lisuke sLisuke))
+                {
+                    ruokaAnnos.Add(sLisuke.ToString());
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Väärä Lisuke, yritä uudelleen.");
+                    continue;
+                }
+            }
+            while (true)
+            {
+                Console.Write($"Kastike ({Kastike.Chili}, {Kastike.Curry}, {Kastike.Hapanimelä}): ");
+                if (Enum.TryParse(Console.ReadLine(), true, out Kastike sKastike))
+                {
+                    ruokaAnnos.Add(sKastike.ToString());
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Väärä Kastike, yritä uudelleen.");
+                    continue;
+                }
+            }
+            Console.WriteLine($"{ruokaAnnos[0]} ja {ruokaAnnos[1]} {ruokaAnnos[2]}-kastikkeella.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Tehtävä 3 - Nuolia kaupan/Nuolia kaupan/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nuolia_kaupan
+{
+    class Program
+    {
+        enum Karki
+        {
+            Puu, Teräs, Timantti
+        }
+        enum Pera
+        {
+            Lehti, Kanansulka, Kotkansulka
+        }
+    static void Main(string[] args)
+        {
+            Console.Write($"Minkälainen kärki ({Karki.Puu}, {Karki.Teräs}, {Karki.Timantti})?: ");
+            string karkivastaus = Console.ReadLine().ToString().ToLower();
+
+            Console.Write($"Minkälaiset sulat ({Pera.Lehti}, {Pera.Kanansulka}, {Pera.Kotkansulka})?: ");
+            string peravastaus = Console.ReadLine().ToString().ToLower();
+
+            Console.Write("Nuolen pituus: (60-100cm): ");
+            int nuolenpituus = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write($"Tämän nuolen hinta on: {PalautaHinta(karkivastaus, peravastaus, nuolenpituus)}");
+            Console.ReadKey();
+        }
+        static int PalautaHinta(string karkiv, string perav, int npituus)
+        {
+            int karkihinta;
+            if (karkiv == Karki.Puu.ToString().ToLower())
+            {
+                karkihinta = 3;
+            }
+            else if (karkiv == Karki.Teräs.ToString().ToLower())
+            {
+                karkihinta = 5;
+            }
+            else
+            {
+                karkihinta = 50;
+            }
+
+            int perahinta;
+            if (perav == Pera.Kanansulka.ToString().ToLower())
+            {
+                perahinta = 1;
+            }
+            else if (perav == Pera.Kotkansulka.ToString().ToLower())
+            {
+                perahinta = 5;
+            }
+            else
+            {
+                perahinta = 0;
+            }
+
+            int pituushinta = (int)(npituus * 0.05);
+            int kokonaishinta = karkihinta + perahinta + pituushinta;
+            return kokonaishinta;
+        }
+    }
+}
